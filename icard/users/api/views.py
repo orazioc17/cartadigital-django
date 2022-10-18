7bbfb14d1496ef7e7e9f70a4837e7f489bcccbc3
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2022-10-18 11:56:43 -0400

    Override creacion de usuario

--- icard/users/api/views.py
+++ icard/users/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from django.contrib.auth.hashers import make_password
 
 from ..models import User
 from .serializers import UserSerializer
@@ -9,3 +10,10 @@ class UserApiViewset(ModelViewSet):
     permission_classes = [IsAdminUser]
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        """
+        Haciendo override del create para que encripte el password del usuario
+        """
+        request.data['password'] = make_password(request.data['password'])
+        return super().create(request, *args, *kwargs)
